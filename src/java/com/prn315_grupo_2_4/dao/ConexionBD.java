b553af5ae362e4fc8be83fdcc4d108d7406288b6
Pechan2892 <113991835+Pechan2892@users.noreply.github.com>
2024-10-29 19:04:26 -0600

    Dev rivas (#1)
    * cambios1
    
    * Remove glassfish-resources.xml from version control
    
    * cambio

---
+++ src/java/com/prn315_grupo_2_4/dao/ConexionBD.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prn315_grupo_2_4.dao;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author edwin
+ */
+class ConexionBD {
+
+		static Connection getConnection() {
+				throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		}
+		
+}
